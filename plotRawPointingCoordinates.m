c0c079ed67e1284ba855e5d3fb83d13eed86432b
ethanknights <ethanknights@hotmail.co.uk>
2021-03-18 21:26:39 +0000

    plot raw pointing coords, works now

--- plotRawPointingCoordinates.m
+++ plotRawPointingCoordinates.m
@@ -1,39 +1,152 @@
 
-thisVarStr = {'M1X','M1Y'};
+%addpath('/imaging/ek03/toolbox/cbrewer')
+close all
 
-%% read in end X/y positions
-[dataX,tmpH] = getData(thisVarStr{1},rawData,sNames);
-[dataY,tmpH] = getData(thisVarStr{1},rawData,sNames);
+nSubs = 11; %D.A is sub 1, controls 2:11
 
-%% Home positions (to minus for every sub: RH then LH)
-home = [-9.3,	124.6, -10.7, 123.7; ... %DA: RH-X RH-Y LH-X LH-Y
-];
+outDir = fullfile('results','rawPointingEndpointCoordinates')
+mkdir(outDir)
 
+%% read X/Y positions
+[M1X,tmpH] = getData('M1X',rawData,sNames); %end
+[sX,tmpH] = getData('sX',rawData,sNames);   %start
 
+[M1Y,tmpH] = getData('M1Y',rawData,sNames);
+[sY,tmpH] = getData('sY',rawData,sNames);
+
+%% plot parameters
+%cmap=cbrewer('seq', 'Reds', 7);
+cmap = lbmap(7,'RedBlue');
+markerSizes = 450;
+targetX = [-250,-150,-100,0,100,150,250];
+targetY = [225,225,225,225,225,225,225];
+
+%% Patient first
 for conditions = 1:4
   
   currConditionName = conditionNames{conditions};
   
+  h = figure('position',[100,100,1200,1200])
+  %set(h,'WindowStyle','docked') % debugging
   
-  %% patient DA
-  subjectLoc = 1;
-  
-  raw_Xcoords = [];
-  raw_Ycoords = [];
-  raw_Xcoords = dataX.targetStack(subjectLoc,:,conditions);
-  raw_Ycoords = dataX.targetStack(subjectLoc,:,conditions);
-      
-  if      conditions == 1 || conditions == 2 %RH
-    for t = 1:7
-      
-      Xcoords(:,t) = home(subjectLoc,1) - raw_Xcoords{t};
-      
-    end
-
+  for subLoc = 1
     
-  elseif  conditions == 3 || conditions == 4 %LH
+    mx = [];
+    sx =[];
+    mx = {M1X.targetStack{subLoc,:,conditions}};
+    sx = {sX.targetStack{subLoc,:,conditions}};
+    
+    my = [];
+    sy =[];
+    my = {M1Y.targetStack{subLoc,:,conditions}};
+    sy = {sY.targetStack{subLoc,:,conditions}};
+    
+    tmpX = [];
+    tmpY = [];
+    for t = 1:7
+      assert(length(mx{t}) == length(sx{t}))
+      tmpX{t} = mx{t} - sx{t};
+      
+      assert(length(my{t}) == length(sy{t}))
+      tmpY{t} = my{t} - sy{t};
+    end
+    
+    %% plot DA's targets
+    clf
+    for t=1:7
+      %%plot
+      scatter(tmpX{t},tmpY{t},markerSizes,...
+        'MarkerFaceColor',cmap(t,:), ...
+        'MarkerEdgeColor',[0 0 0],...
+        'MarkerFaceAlpha',0.75,'MarkerEdgeAlpha',1,...
+        'LineWidth',2)
+      hold on
+      %%plot target references
+      scatter(targetX(t),targetY(t),600,'black','o')
+    end
+    setPlotParameters
+    
+    %% save plot
+    outName = fullfile(outDir,['patient_',currConditionName]);
+    cmdStr = sprintf('export_fig %s.png -transparent',outName)
+    eval(cmdStr);
+    
+    h=gcf;
+    savefig(h,[outName,'.fig']);
     
   end
   
-  
 end
+
+close all
+
+%% Repeat with control group
+for conditions = 1:4
+  
+  currConditionName = conditionNames{conditions};
+  
+  h = figure('position',[100,100,1200,1200])
+  %set(h,'WindowStyle','docked') % debugging
+  tmpX = [];
+  tmpY = [];
+  
+  for subLoc = 2:11
+    
+    mx = [];
+    sx =[];
+    mx = {M1X.targetStack{subLoc,:,conditions}};
+    sx = {sX.targetStack{subLoc,:,conditions}};
+    
+    my = [];
+    sy =[];
+    my = {M1Y.targetStack{subLoc,:,conditions}};
+    sy = {sY.targetStack{subLoc,:,conditions}};
+    
+
+    for t = 1:7
+      assert(length(mx{t}) == length(sx{t}))
+      tmpX(subLoc-1,t) = mean(mx{t} - sx{t}); %%!!take mean here for controls!!
+      %variabilityX{subLoc-1,t} = std(mx{t} - sx{t}); %%!! also get controls variability!!
+      
+      assert(length(my{t}) == length(sy{t}))
+      tmpY(subLoc-1,t) = mean(my{t} - sy{t}); %%!!take mean here for controls!!
+      %variabilityY{subLoc-1,t} = std(my{t} - sy{t}); %%!! also get controls variability!!
+    end
+    
+  end
+  
+    %% plot mean of each control (target size as a function of variability?)
+    clf
+    for t=1:7
+      %%plot
+      scatter(tmpX(:,t),tmpY(:,t),markerSizes,...
+        'MarkerFaceColor',cmap(t,:), ...
+        'MarkerEdgeColor',[0 0 0],...
+        'MarkerFaceAlpha',0.75,'MarkerEdgeAlpha',1,...
+        'LineWidth',2)
+      hold on
+      %%plot target references
+      scatter(targetX(t),targetY(t),600,'black','o')
+    end
+    setPlotParameters
+    
+    %% save plot
+    outName = fullfile(outDir,['controlGroupMeans_',currConditionName]);
+    cmdStr = sprintf('export_fig %s.png -transparent',outName)
+    eval(cmdStr);
+    
+    h=gcf;
+    savefig(h,[outName,'.fig']);
+    
+  end
+  
+% end
+
+
+
+
+
+return
+
+
+
---
+++ setPlotParameters.m
@@ -0,0 +1,8 @@
+%%quickly edit pointing plot parameters
+xlabel('Normalised X Endpoint Coordinate (mm)');
+ylabel('Normalised Y Endpoint Coordinate (mm)');
+xlim([-300 300]);
+ylim([150 275]);
+set(gca,'box','off','color','none','TickDir','out','fontsize',18);
+title(currConditionName);
+grid on
